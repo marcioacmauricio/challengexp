92719af272c7e081b3500757f4fd4098cf64a999
Marcio Maurício <marcioacmauricio@gmail.com>
2019-07-18 18:47:59 -0300

    Start Project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './assets/css/style.css'
+
+
+const Index = () => {
+  return <div className="border_solid" >React Spotfy Player!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+          test: /\.scss$/,
+          use: [
+              {
+                  loader: 'style-loader'
+              },
+              {
+                  loader: 'css-loader'
+              },
+              {
+                  loader: 'sass-loader',
+              }
+          ]
+      },
+      {
+          test: /\.css$/,
+          use: [
+              {
+                  loader: 'style-loader'
+              },
+              {
+                  loader: 'css-loader'
+              }
+          ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
